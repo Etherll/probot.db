456e12fdeae9b82faaddc2790f1156efb80c00d9
Etherll <61019402+Etherll@users.noreply.github.com>
2021-04-09 12:43:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
+import setuptools
 from distutils.core import setup
 
 with open("README.md", "r") as fh:
   long_description = fh.read()
 setup(
   name = 'probot.db',         # How you named your package folder (MyLib)
-  packages = ['probot.db'],   # Chose the same as "name"
+  packages = ['probotdb'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'probot.db is the best data base ever uses probot database :shitoh:',   # Give a short description about your library
+  description = 'ProBot embed messages feature based database, :shitoh:',   # Give a short description about your library
   long_description= long_description,
   author = 'Etherl',                   # Type in your name
   author_email = 'None@domain.com',      # Type in your E-Mail
   url = 'https://github.com/Etherll/probot.db',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/Etherll/probot.db',    # I explain this later on
-  keywords = ['ok', 'ok anime', 'anime'],   # Keywords that define your package best
+  keywords = ['probotdb', 'probot', 'db'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'requests',
-          'bs4',
+          'requests'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
